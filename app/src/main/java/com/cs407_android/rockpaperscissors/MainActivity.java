5032d8f4f37b3cfebe9adaba0d4f94628aa59c14
Eric Anderson <eanderson608@gmail.com>
2016-02-22 15:06:46 -0600

    added action bar

--- app/src/main/java/com/cs407_android/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/cs407_android/rockpaperscissors/MainActivity.java
@@ -3,13 +3,10 @@ package com.cs407_android.rockpaperscissors;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-
-import com.xgc1986.ripplebutton.widget.RippleButton;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         if (savedInstanceState != null) {
             return;
